9fd7fb62bf09668d9c54946fa46972249360befd
Kushan Joshi <0o3ko0@gmail.com>
2016-02-14 18:35:41 +0530

    Update 2016-02-14T13:05:36.016Z

--- app/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import gll from '../data/data.json!json';
-import './test.css!css';
-export default () => {
-  // var x = () => 4;
-  console.log(JSON.parse(gll));
-  return <div>hello world</div>;
-};
